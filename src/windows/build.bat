d1350dc3da8aed5b9a8f34bccf5a227e67015302
russiandesman <des.maryin@googlemail.com>
2024-09-10 15:12:26 +0300

    windows: add manifest to executable for high DPI awareness

--- src/windows/build.bat
+++ src/windows/build.bat
@@ -1,4 +1,5 @@
 cd /D "%~dp0"
 mkdir obj
 cl /Foobj\ /Fe:warpd.exe *.c icon.res ..\config.c ..\daemon.c ..\grid.c ..\grid_drw.c ..\hint.c ..\histfile.c ..\history.c ..\input.c ..\mode-loop.c ..\mouse.c ..\normal.c ..\screen.c ..\scroll.c ..\platform\windows\*.c user32.lib gdi32.lib shell32.lib
+mt.exe -manifest warpd.exe.manifest -outputresource:warpd.exe;1
 rmdir /s /q obj
